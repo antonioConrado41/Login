5c8cb1d4b1d987208fd54590f0f080da1b43d967
Antonio Conrado <antoniojconradojunior@hotmail.com>
2020-11-12 14:02:55 +0000

    dashboard, login and register

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', (req, res) => res.send('Login'))
+router.get('/login', (req, res) => res.render('Login'))
 
-router.get('/register', (req, res) => res.send('Register'))
+router.get('/register', (req, res) => res.render('Register'))
 
 
 module.exports = router;
