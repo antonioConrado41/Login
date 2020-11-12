1424c4749e39a18c647ee0f9172f94978d9cfe3b
Antonio Conrado <antoniojconradojunior@hotmail.com>
2020-11-12 14:49:46 +0000

    connecting to DB

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//db config
+const db = require('./config/keys').MongoURI;
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology:true})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log(err))
 
 //ejs
 app.use(expressLayouts);
---
+++ config/keys.js
@@ -0,0 +1,2 @@
+module.exports = {MongoURI: 'mongodb+srv://user:pass1234!@cluster0.r5pvi.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
